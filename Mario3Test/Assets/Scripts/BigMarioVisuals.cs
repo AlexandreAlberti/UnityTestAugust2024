cfa64880fd4874b173fc18c60ad39481fceef318
AlexandreAlberti <alberti.bu@gmail.com>
2024-07-26 12:01:40 +0200

    Run-Walking animations

---
+++ Mario3Test/Assets/Scripts/BigMarioVisuals.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigMarioVisuals : MonoBehaviour
+{
+    private const string MARIO_RUN = "Running";
+    private const string MARIO_RUN_MAX_SPEED = "MaxSpeed";
+
+    [SerializeField] private BigMarioMovement _bigMarioMovement;
+    [SerializeField] private Animator _animator;
+
+    private void Awake() {
+        _bigMarioMovement.OnStop += BigMarioMovement_OnStop;
+        _bigMarioMovement.OnRun += BigMarioMovement_OnRun;
+        _bigMarioMovement.OnRunMaxSpeed += BigMarioMovement_OnRunMaxSpeed;
+    }
+
+    private void BigMarioMovement_OnRun(object sender, float animationSpeed) {
+        _animator.speed = Mathf.Max(1.0f, animationSpeed);
+        _animator.SetBool(MARIO_RUN, true);
+        _animator.SetBool(MARIO_RUN_MAX_SPEED, false);
+    }
+
+    private void BigMarioMovement_OnRunMaxSpeed(object sender, float animationSpeed) {
+        _animator.speed = animationSpeed;
+        _animator.SetBool(MARIO_RUN, true);
+        _animator.SetBool(MARIO_RUN_MAX_SPEED, true);
+    }
+
+    private void BigMarioMovement_OnStop(object sender, System.EventArgs e) {
+        _animator.SetBool(MARIO_RUN, false);
+        _animator.SetBool(MARIO_RUN_MAX_SPEED, false);
+    }
+}
